91e638f6f6d743e48d4be9ce87856500c525c2a0
Alex Goncharenko <darkstar481@gmail.com>
2021-02-17 20:10:27 -0700

    Added LM

--- src/helpers/testFunction.js
+++ src/helpers/testFunction.js
@@ -1,8 +1,12 @@
 import { sleep } from "./sleep";
+import { fittedParams } from "./lm";
 
 export const testFunction = async (uwi, color) => {
   console.log("testFunction: about to sleep");
   await sleep(1000);
   console.log("testFunction: the sleep is over");
+
+  console.log("fitted params from LM", fittedParams);
+
   return { uwi, color };
 };
